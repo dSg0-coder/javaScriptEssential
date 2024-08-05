d6330cfc14955bb01dc624574f14b5d617bfa352
Your Name <you@example.com>
2024-08-05 04:12:52 -0400

    Added code to scope_lab.js

--- scope_lab.js
+++ scope_lab.js
@@ -10,3 +10,14 @@ var blockVar = "I'm a block-scoped var";
 let blockLet = "I'm a block-scoped let";
 const blockConst = "I'm a block-scoped const";
 }
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
